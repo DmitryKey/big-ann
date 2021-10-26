26349b7088e13e633151a2b864f11c99c24ea354
DmitryKey <dmitry.kan@gmail.com>
2021-10-26 23:02:05 +0300

    #4 indexing wrapper script

--- src/run_kanndi.sh
+++ src/run_kanndi.sh
@@ -7,7 +7,7 @@ export PYTHONPATH=.
 # 100M points
 #python algorithms/sharding/kanndi/shard_by_distance.py --input_file /datadrive/big-ann-benchmarks/data/bigann.bak/base.1B.u8bin.crop_nb_100000000 --output_dir /datadrive/big-ann/data -M 100
 # 1B points
-python algorithms/sharding/kanndi/shard_by_distance.py --input_file /datadrive/big-ann-benchmarks/data/bigann/base.1B.u8bin --output_dir /datadrive/big-ann/index/bigann/data.1B -M 100 --dtype uint8
+#python algorithms/sharding/kanndi/shard_by_distance.py --input_file /datadrive/big-ann-benchmarks/data/bigann/base.1B.u8bin --output_dir /datadrive/big-ann/index/bigann/data.1B/ -M 1000 --dtype uint8
 
 
 ###
@@ -19,4 +19,4 @@ python algorithms/sharding/kanndi/shard_by_distance.py --input_file /datadrive/b
 # SSNPP: Facebook SimSearchNet++
 ###
 # CONVERGED
-# python algorithms/sharding/kanndi/shard_by_distance.py --input_file /datadrive/big-ann-benchmarks/data/FB_ssnpp/FB_ssnpp_database.u8bin.crop_nb_100000000 --output_dir /datadrive/big-ann/index/ssnpp/data.100M/ -M 100 --dtype uint8
+python algorithms/sharding/kanndi/shard_by_distance.py --input_file /datadrive/big-ann-benchmarks/data/FB_ssnpp/FB_ssnpp_database.u8bin.crop_nb_100000000 --output_dir /datadrive/big-ann/index/ssnpp/data.100M/ -M 100 --dtype uint8
